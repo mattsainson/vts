8792030ead811a1714bedb0b8a9e01fb93c9c5c1
Matt Sainson <mattsainson@gmail.com>
2019-04-06 15:51:05 -0700

    Fixing lint

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var env = require("dotenv").config();
+require("dotenv").config();
 var express = require("express");
 // var exphbs = require("express-handlebars");
 var passport = require("passport");
