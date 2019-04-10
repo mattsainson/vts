0fd2ca9d1f5f32f72409216cc4c5975db1320e7a
Kate Burson <burson.kate@gmail.com>
2019-04-10 10:13:01 -0700

    rebase

--- public/js/signup.js
+++ public/js/signup.js
@@ -12,13 +12,11 @@ function signUp() {
     password : $('#password').val().trim(),
     isTutor: $('#isTutor').children('option:selected').val()
   };
-    $.post('/signup', userObj, function(err, res) {
-      if (err) {
-        throw (err);
-      } else {
-        userObj.id = res.id;
-        redirect('/dashboard.html');
-        return('Success: Status(200)');
-      }
-    });
+  $.post('/signup', userObj, function(err, res) {
+    if (err) {
+      throw (err);
+    } else {
+      userObj.id = res.id;
+    }
+  });
 }
